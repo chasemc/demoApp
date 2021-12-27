1f91dbdc086667cd784046afa3602691f72664da
Chase Clark <18691127+chasemc@users.noreply.github.com>
2021-12-27 07:10:46 -0600

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -5,7 +5,7 @@
 #' @importFrom shiny shinyApp
 #' @export
 run_app <- function(options = list()) {
-  shinyApp(ui = app_ui,
+  shiny::shinyApp(ui = app_ui,
            server = app_server,
            options = options) 
   
